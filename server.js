3ace67f7745692783c898325b56211100779e5fe
Bruno Melo <brunomelod@gmail.com>
2026-01-30 09:42:39 -0300

    Criação do arquivo .env e do arquivo server.js com a configuração da porta do servidor.

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv/config";
+
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`O servidor está rodando na porta ${PORT}`);
+});
