05fccca34a104523707b007364ac8e29ffad3d8e
Bruno Melo <brunomelod@gmail.com>
2026-01-30 12:26:49 -0300

    refactor: configura app.js com rotas e startServer em server.js para conexões aos bancos.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import userRoutes from "./src/routes/userRoutes.js";
 import authRoutes from "./src/routes/authRoutes.js";
+import postRoutes from "./src/routes/postRoutes.js";
+import commentRoutes from "./src/routes/commentRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
 
 export default app;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/mongodb.js"; 
+import { connectDB as connectDBPostgres } from "./src/config/postgres.js";
+import { sequelize } from "./src/models/index.js";
 
 const startServer = async () => {
   await connectDB();
+  await connectDBPostgres();
+
+  await sequelize.sync();
+
   const PORT = process.env.PORT;
 
   app.listen(PORT, () => {
@@ -11,4 +17,7 @@ const startServer = async () => {
   });
 };
 
-startServer();
+startServer().catch(error => {
+  console.error('Erro ao iniciar o servidor:', error);
+  process.exit(1);
+});
