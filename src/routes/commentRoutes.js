e1432ba392784b5e729517f7c1228678196599fb
Bruno Melo <brunomelod@gmail.com>
2026-01-30 13:09:05 -0300

    fix: corrige import do authMiddleware nas rotas e move o app.js para /src.

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { authMiddleware } from '../middlewares/authMiddlewares.js';
 import { addComment, deleteComment } from '../controllers/commentController.js';
 
 const router = express.Router();
