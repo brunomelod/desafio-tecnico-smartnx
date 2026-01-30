811769c7e227d9739675e591925c88de8b3996eb
Bruno Melo <brunomelod@gmail.com>
2026-01-30 09:52:45 -0300

    feat: adiciona configuração do MongoDB e model de usuário

---
+++ src/config/mongodb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MongoDB: Conexão com o banco realizado com sucesso");
+    } catch (error) {
+        console.error("MongoDB: Erro ao conectar com o banco", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
